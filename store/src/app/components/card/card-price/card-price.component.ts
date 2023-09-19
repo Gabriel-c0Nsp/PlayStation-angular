6d775a6c0310080e2cfa356cb8a2c6708b37d1a8
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-09-18 21:11:27 -0300

    Implementado card price e suas respectivas estilizações

--- store/src/app/components/card/card-price/card-price.component.ts
+++ store/src/app/components/card/card-price/card-price.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-price',
@@ -8,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class CardPriceComponent implements OnInit {
 
   constructor() { }
+
+  @Input()
+  gameType: string = 'Digital PS4'
+  @Input()
+  gamePrice: string = 'RS 129,90'
 
   ngOnInit(): void {
   }
